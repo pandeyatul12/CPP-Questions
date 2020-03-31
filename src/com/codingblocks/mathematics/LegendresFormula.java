d316bea59e15187bfc1f21de084693f0cf831dc4
Kunal Kushwaha <kunalkushwaha453@gmail.com>
2020-04-01 01:51:03 +0530

    mathematics

---
+++ src/com/codingblocks/mathematics/LegendresFormula.java
@@ -0,0 +1,23 @@
+package com.codingblocks.mathematics;
+
+import java.util.Scanner;
+
+public class LegendresFormula {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int t = s.nextInt();
+        for (int i = 0; i < t; i++) {
+            int n = s.nextInt();
+            int k = s.nextInt();
+            System.out.println(findX(n, k));
+         }
+    }
+    public static int findX(int n, int p) {
+        int x = 0;
+        while(n > 0) {
+            n /= p;
+            x += n;
+        }
+        return x;
+    }
+}
