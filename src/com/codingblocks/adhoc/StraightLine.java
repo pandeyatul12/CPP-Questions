125c2a35ee5ac1904bdbfc1415e86b1ca6ebdae6
Kunal Kushwaha <kunalkushwaha453@gmail.com>
2020-05-11 21:48:19 +0530

    inout3

---
+++ src/com/codingblocks/adhoc/StraightLine.java
@@ -0,0 +1,35 @@
+package com.codingblocks.adhoc;
+
+import java.util.Scanner;
+
+public class StraightLine {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int[][] coordinates = {{1,2},{2,3},{3,4},{4,5},{5,6},{6,7}};
+        System.out.println(checkStraightLine(coordinates));
+    }
+
+    public static boolean checkStraightLine(int[][] coordinates) {
+        int[] first = coordinates[0];
+        int[] second = coordinates[1];
+        Integer s = slope(first, second);
+        for (int i = 2; i < coordinates.length; i++) {
+            Integer s2 = slope(coordinates[i], second);
+            if (s2 == null){
+                return false;
+            }
+            if (!s2.equals(s)){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static Integer slope(int[] first, int[] second){
+        if (second[0] - first[0] == 0){
+            return null;
+        }
+        return (second[1] - first[1])/(second[0] - first[0]);
+    }
+
+}
