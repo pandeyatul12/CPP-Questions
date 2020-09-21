fa54412fcaf332e25ea8cec74d117e5b457ed061
Kunal Kushwaha <kunalkushwaha453@gmail.com>
2020-09-21 18:45:32 +0530

    topK

---
+++ src/com/kunal/topKElements/RearrangeString.java
@@ -0,0 +1,59 @@
+package com.kunal.topKElements;
+
+import java.util.*;
+
+public class RearrangeString {
+    public static void main(String[] args) {
+        String str = "Programming";
+        System.out.println(ans(str));
+    }
+
+    public static String ans(String str) {
+        Map<Character, Integer> map = new HashMap<>();
+        for (char ch : str.toCharArray()) {
+            map.put(ch, map.getOrDefault(ch, 0) + 1);
+        }
+        PriorityQueue<Map.Entry<Character, Integer>> maxHeap = new PriorityQueue<>(Comparator.comparingInt(o -> -o.getValue()));
+        maxHeap.addAll(map.entrySet());
+        Map.Entry<Character, Integer> previous = null;
+        StringBuilder builder = new StringBuilder();
+        while (!maxHeap.isEmpty()) {
+            Map.Entry<Character, Integer> current = maxHeap.remove();
+            if (previous != null && previous.getValue() > 0) {
+                maxHeap.add(previous);
+            }
+            builder.append(current.getKey());
+            current.setValue(current.getValue() - 1);
+            previous = current;
+        }
+        return builder.length() == str.length() ? builder.toString() : "";
+    }
+
+    // if characters need tp be K distance apart
+    public static String ansKDist(String str, int k) {
+        if (k <= 1) {
+            return str;
+        }
+        Map<Character, Integer> map = new HashMap<>();
+        for (char ch : str.toCharArray()) {
+            map.put(ch, map.getOrDefault(ch, 0) + 1);
+        }
+        PriorityQueue<Map.Entry<Character, Integer>> maxHeap = new PriorityQueue<>(Comparator.comparingInt(o -> -o.getValue()));
+        maxHeap.addAll(map.entrySet());
+        Queue<Map.Entry<Character, Integer>> queue = new LinkedList<>();
+        StringBuilder builder = new StringBuilder();
+        while (!maxHeap.isEmpty()) {
+            Map.Entry<Character, Integer> current = maxHeap.remove();
+            builder.append(current.getKey());
+            current.setValue(current.getValue() - 1);
+            queue.add(current);
+            if (queue.size() == k) {
+                Map.Entry<Character, Integer> removed = queue.remove();
+                if (removed.getValue() > 0) {
+                    maxHeap.add(removed);
+                }
+            }
+        }
+        return builder.length() == str.length() ? builder.toString() : "";
+    }
+}
---
+++ src/com/kunal/topKElements/TaskScheduler.java
@@ -0,0 +1,41 @@
+package com.kunal.topKElements;
+
+import java.util.*;
+
+public class TaskScheduler {
+    public static void main(String[] args) {
+        char[] tasks = new char[] { 'a', 'a', 'a', 'b', 'c', 'c' };
+        System.out.println("Minimum intervals needed to execute all tasks: " + scheduleTasks(tasks, 2));
+
+        tasks = new char[] { 'a', 'b', 'a' };
+        System.out.println("Minimum intervals needed to execute all tasks: " + scheduleTasks(tasks, 3));
+    }
+    public static int scheduleTasks(char[] tasks, int k) {
+        Map<Character, Integer> map = new HashMap<>();
+        for(char task : tasks) {
+            map.put(task,  map.getOrDefault(task, 0) + 1);
+        }
+        PriorityQueue<Map.Entry<Character, Integer>> maxHeap = new PriorityQueue<>(Comparator.comparingInt(o -> o.getValue()));
+        maxHeap.addAll(map.entrySet());
+        int ans = 0;
+        while (!maxHeap.isEmpty()) {
+            List<Map.Entry<Character, Integer>> waitList = new ArrayList<>();
+            int n = k+1;
+            // take top k+1 elements
+            while (n > 0 && !maxHeap.isEmpty()) {
+                Map.Entry<Character, Integer> removed = maxHeap.remove();
+                ans++;
+                if (removed.getValue() > 1) {
+                    removed.setValue(removed.getValue() - 1);
+                    waitList.add(removed);
+                }
+                n--;
+            }
+            maxHeap.addAll(waitList);
+            if (!maxHeap.isEmpty()) {
+                ans += n;
+            }
+        }
+        return ans;
+    }
+}
