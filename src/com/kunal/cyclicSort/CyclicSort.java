605802ae71595fd6dddc7ecc613fde3107797039
Kunal Kushwaha <kunalkushwaha453@gmail.com>
2020-10-16 14:39:02 +0530

    cycle sort

--- src/com/kunal/cyclicSort/CyclicSort.java
+++ src/com/kunal/cyclicSort/CyclicSort.java
@@ -1,16 +1,11 @@
 package com.kunal.cyclicSort;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+class CyclicSort {
 
-public class CyclicSort {
-    // O(N) TIME COMPLEXITY: O(N) + O(N-1)
     public static void sort(int[] nums) {
         int i = 0;
         while (i < nums.length) {
             int j = nums[i] - 1;
-            // check if num is at index num-1
             if (nums[i] != nums[j])
                 swap(nums, i, j);
             else
@@ -24,51 +19,23 @@ public class CyclicSort {
         arr[j] = temp;
     }
 
-    // OR
-    public static void sort2(int[] nums) {
-        for (int i = 0; i < nums.length; i++) {
-            while (nums[i] != nums[nums[i]-1]) {
-                // swap nums[i] with index nums[i]-1
-                int temp = nums[i];
-                nums[i] = nums[nums[i] - 1];
-                nums[temp - 1] = temp;
-            }
-        }
-    }
+    public static void main(String[] args) {
+        int[] arr = new int[]{3, 1, 5, 4, 2};
+        CyclicSort.sort(arr);
+        for (int num : arr)
+            System.out.print(num + " ");
+        System.out.println();
 
-    public static int findMissingNumber(int[] nums) {
-        for (int i = 0; i < nums.length; i++) {
-            while (nums[i] < nums.length && nums[i] != nums[nums[i]]){
-                int temp = nums[i];
-                nums[i] = nums[nums[i]];
-                nums[temp] = temp;
-            }
-        }
-        for (int i = 0; i < nums.length; i++) {
-            if (nums[i] != i){
-                return i;
-            }
-        }
-        return nums.length;
-    }
+        arr = new int[]{2, 6, 4, 3, 1, 5};
+        CyclicSort.sort(arr);
+        for (int num : arr)
+            System.out.print(num + " ");
+        System.out.println();
 
-    public static List<Integer> findNumbers(int[] nums) {
-        List<Integer> missingNumbers = new ArrayList<>();
-
-        for (int i = 0; i < nums.length; i++) {
-            while (nums[i] != nums[nums[i]-1]){
-                // swap
-                int temp = nums[i];
-                nums[i] = nums[nums[i] - 1];
-                nums[temp - 1] = temp;
-            }
-        }
-        for (int i = 0; i < nums.length; i++) {
-            if (nums[i] != i-1){
-                missingNumbers.add(i+1);
-            }
-        }
-
-        return missingNumbers;
+        arr = new int[]{1, 5, 6, 4, 3, 2};
+        CyclicSort.sort(arr);
+        for (int num : arr)
+            System.out.print(num + " ");
+        System.out.println();
     }
 }
