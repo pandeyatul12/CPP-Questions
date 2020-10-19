bc7d75bcd3d41c4504272a1fe1b5ce7c265e565a
Kunal Kushwaha <kunalkushwaha453@gmail.com>
2020-10-19 23:13:17 +0530

    etc

--- src/com/kunal/binarySearch/SearchInfiniteSortedArray.java
+++ src/com/kunal/binarySearch/SearchInfiniteSortedArray.java
@@ -24,7 +24,6 @@ public class SearchInfiniteSortedArray {
                 return mid;
             }
         }
-
         return -1;
     }
 
--- src/com/kunal/binarySearch/SmallestInSortedMatrix.java
+++ src/com/kunal/binarySearch/SmallestInSortedMatrix.java
@@ -78,7 +78,7 @@ public class SmallestInSortedMatrix {
 
     public static void main(String[] args) {
         int n = 4, m = 5, x = 8;
-        int mat[][] = {{0, 6, 8, 9, 11},
+        int[][] mat = {{0, 6, 8, 9, 11},
                 {20, 22, 28, 29, 31},
                 {36, 38, 50, 61, 63},
                 {64, 66, 100, 122, 128}};
